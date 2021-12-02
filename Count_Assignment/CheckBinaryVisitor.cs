41e981b2f6c7e2c01a1fd815377b4e9cb5570c13
Garima Chopra <44365159+GarimaChopra@users.noreply.github.com>
2021-12-02 00:21:30 -0500

    Add files via upload

---
+++ Count_Assignment/CheckBinaryVisitor.cs
@@ -0,0 +1,53 @@
+/**
+    \file   CheckBinaryVisitor.cs
+    \brief  Contains Functions definition.
+    \author Garima Chopra
+
+ */
+//----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//----------------------------------------------------------------------
+namespace CSImageViewer {
+    public class CheckBinaryVisitor {
+        private bool isBinary = false;
+        private int  min      = 255;
+        private int  max      = 0;
+
+ 	/** \brief  <b> This method checks if image is binary image</b>
+         *  
+         *  \param  g        object of GrayImageData
+         * 
+         *
+         *
+         *  \returns  sets value of isBinary,min,max
+         */
+
+        public void visit ( GrayImageData g ) {
+
+            int size=g.getW()*g.getH();     //size of array for gray image
+
+            for (int i = 0; i < size; i++) {
+                if (g.getData( i ) < min) min = g.getData( i ); //if value less than min swap
+                if (g.getData( i ) > max) max = g.getData( i ); //if value greater than max swap
+            }
+            
+            for (int i = 0; i < size; i++) {
+                if ((g.getData( i ) == min) || (g.getData( i ) == max)) { //if value is equal to max or min image is binary
+                    isBinary = true;
+
+                } else {
+                    isBinary = false;
+                    break;
+                }
+            }
+        
+        }
+
+        public bool get ( ) { return isBinary; }
+        public int getMin ( ) { return min; }
+        public int getMax ( ) { return max; }
+    }
+}
---
+++ Count_Assignment/CountObjects.cs
@@ -0,0 +1,93 @@
+﻿/**
+    \file   CountObjects.cs
+    \brief  Contains Functions definition.
+    \author Garima Chopra
+ */
+//----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//----------------------------------------------------------------------
+namespace CSImageViewer {
+    public class CountObjects {
+
+        private int externalCount = 0;  //<<< count of external corners
+        private int internalCount = 0;  //<<< count of internal corners
+        private int objectCount   = 0;  //<<< count of objects
+
+        private bool isBinary     = false;
+        private bool isBorderOK   = false;
+        private bool is4Connected = false;
+
+        public void visit ( GrayImageData g ) {
+
+            int row=g.getH();
+            int col = g.getW();
+            CheckBinaryVisitor  cb=new  CheckBinaryVisitor();
+            CheckBorderEmptyVisitor cb1=new CheckBorderEmptyVisitor();
+            Check4ConnectedVisitor  ck=new  Check4ConnectedVisitor();
+            cb.visit( g );
+            cb1.visit( g );
+            ck.visit( g );
+            int min=cb.getMin();
+            int max=cb.getMax();
+            isBinary = cb.get();
+            isBorderOK = cb1.get();
+            is4Connected = ck.get();
+            if ((cb.get() == true)  && (cb1.get()==true)  &&  (ck.getBadCount()==0) &&  (ck.get()==true) )    //check image is binary ,empty border and fully 4 connected
+                {
+                for(int r=0;r<row-1;r++) 
+                    {
+                        for (int c = 0; c < col - 1; c++) 
+                        {
+                            if ((g.getGray( r, c ) == min) && (g.getGray( r + 1, c ) == min) && (g.getGray( r, c + 1 ) == min) && (g.getGray( r + 1, c + 1 ) == max)) //pattern matching
+                            {
+                            externalCount += 1;
+                             } 
+                            else if ((g.getGray( r, c ) == min) && (g.getGray( r + 1, c ) == min) && (g.getGray( r, c + 1 ) == max) && (g.getGray( r + 1, c + 1 ) == min))//pattern matching
+                            {
+                            externalCount += 1;
+                            } 
+                            else if ((g.getGray( r, c ) == min) && (g.getGray( r + 1, c ) == max) && (g.getGray( r, c + 1 ) == min) && (g.getGray( r + 1, c + 1 ) == min))//pattern matching
+                            {
+                            externalCount += 1;
+                            } 
+                            else if ((g.getGray( r, c ) == max) && (g.getGray( r + 1, c ) == min) && (g.getGray( r, c + 1 ) == min) && (g.getGray( r + 1, c + 1 ) == min))//pattern matching
+                            {
+                            externalCount += 1;
+                            } 
+                            else if ((g.getGray( r, c ) == max) && (g.getGray( r + 1, c ) == max) && (g.getGray( r, c + 1 ) == max) && (g.getGray( r + 1, c + 1 ) == min))//pattern matching
+                            {
+                            internalCount += 1;
+                            }
+                            else if ((g.getGray( r, c ) == max) && (g.getGray( r + 1, c ) == max) && (g.getGray( r, c + 1 ) == min) && (g.getGray( r + 1, c + 1 ) == max)) //pattern matching
+                            {
+                            internalCount += 1;
+                            } 
+                            else if ((g.getGray( r, c ) == max) && (g.getGray( r + 1, c ) == min) && (g.getGray( r, c + 1 ) == max) && (g.getGray( r + 1, c + 1 ) == max))//pattern matching
+                            {
+                            internalCount += 1;
+                            } 
+                            else if ((g.getGray( r, c ) == min) && (g.getGray( r + 1, c ) == max) && (g.getGray( r, c + 1 ) == max) && (g.getGray( r + 1, c + 1 ) == max)) //pattern matching
+                            {
+                            internalCount += 1;
+                            }
+
+                        }
+                       
+                    }
+                objectCount = (externalCount - internalCount) / 4;  //object count
+            }
+            
+        }
+        public int get ( ) { return objectCount; }
+        public int getInternal ( ) { return internalCount; }
+        public int getExternal ( ) { return externalCount; }
+
+        public bool getIsBinary ( ) { return isBinary; }
+        public bool getIsBorderOK ( ) { return isBorderOK; }
+        public bool getIs4Connected ( ) { return is4Connected; }
+    }
+}
